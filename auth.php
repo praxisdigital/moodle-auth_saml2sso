14a28745386200d9f46f0ef2c1c76c98049160c0
Marco Ferrante <marco@csita.unige.it>
2024-12-02 18:19:08 +0100

    Better SSP version check

--- auth.php
+++ auth.php
@@ -571,7 +571,11 @@ class auth_plugin_saml2sso extends auth_plugin_base {
 
         require($this->config->sp_path . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . '_autoload.php');
         $sspconfig = \SimpleSAML\Configuration::getInstance();
-        if (version_compare($sspconfig->getVersion(), '2.0.2') < 0) {
+        if (version_compare($sspconfig->getVersion(), '2.0.0') < 0) {
+            echo $OUTPUT->notification('SimpleSAMLphp lib version ('
+                    . $sspconfig->getVersion() . ') is not supported, this plugin requires a 2.x release', \core\output\notification::NOTIFY_ERROR);
+        }
+        elseif (version_compare($sspconfig->getVersion(), '2.0.2') < 0) {
             echo $OUTPUT->notification('SimpleSAMLphp lib seems too old ('
                     . $sspconfig->getVersion() . ') and insicure: you should upgrade it', \core\output\notification::NOTIFY_WARNING);
         }
